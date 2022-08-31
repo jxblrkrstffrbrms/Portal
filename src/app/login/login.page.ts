06f73f8b8a5fe06e8a21faec5fc7a90c653df466
jxblrkrstffrbrms <jaxblairekristofferramos@gmail.com>
2022-08-31 20:09:29 +0800

    minor revisions on tabs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -15,7 +15,7 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(8)]],
+      name: ['', [Validators.required, Validators.minLength(5)]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
     })
   }
