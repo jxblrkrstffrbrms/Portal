11463a04cdfb7b9970f2b1b064dd649f1fd8c583
jxblrkrstffrbrms <jaxblairekristofferramos@gmail.com>
2022-08-31 19:27:51 +0800

    finished the login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  ionicForm: FormGroup;
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(private router:Router, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    })
+  }
+
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+    }
+  }
+
+  home(){
+    this.router.navigate(['/tabs'])
   }
 
 }
